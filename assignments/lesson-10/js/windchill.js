74029170b912ddb5a9d087f75356163d5efbb216
zachandalex <chu18013@byui.edu>
2019-06-29 20:26:27 -0700

    .

--- assignments/lesson-10/js/windchill.js
+++ assignments/lesson-10/js/windchill.js
@@ -1,6 +1,6 @@
-var tHigh = parseInt(document.getElementById("value2").value);
-var sSpeed = parseInt(document.getElementById("value5").value);
+var tHigh = parseInt(document.getElementById("high").innerHTML);
+var sSpeed = parseInt(document.getElementById("speed").innerHTML);
 var fWindchill = 35.74 + (0.6215*tHigh)-(35.75*Math.pow(sSpeed, 0.16))+(0.4275*tHigh*Math.pow(sSpeed,0.16));
-document.getElementById("value3").innerHTML = fWindchill.toFixed(0);
+document.getElementById("windchill").innerHTML = fWindchill.toFixed(0);
 
 
