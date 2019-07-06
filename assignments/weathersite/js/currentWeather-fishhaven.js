26a31c358e9383ce3430387b511f6305b8f1ddb6
zachandalex <chu18013@byui.edu>
2019-07-06 15:20:09 -0700

    .

--- assignments/weathersite/js/currentWeather-fishhaven.js
+++ assignments/weathersite/js/currentWeather-fishhaven.js
@@ -13,6 +13,7 @@ weatherRequest.onload = function()
         var sSpeed = parseInt(document.getElementById("speed").innerHTML=weatherData.wind.speed);
         var fWindchill = 35.74 + (0.6215*tHigh)-(35.75*Math.pow(sSpeed, 0.16))+(0.4275*tHigh*Math.pow(sSpeed,0.16));
         document.getElementById("windchill").innerHTML = fWindchill.toFixed(0);
+        document.getElementById('currenttemp').innerHTML = weatherData.main.temp;
     }
 
     
